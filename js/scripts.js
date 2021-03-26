eb24e6910b1c03fd37b84b24e9a56b68417addb9
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-03-26 14:39:22 -0700

    add text styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-  $("form#language").submit(function(event) {
+  $("form#language").click(function(event) {
     const game = parseInt($("select#game").val());
     const country = parseInt($("select#country").val());
     const os = parseInt($("select#os").val());
     const music = parseInt($("select#music").val());
     const food = parseInt($("select#food").val());
     let value = (game + country + os + music + food);
-    alert(value);
     if (value >= 5 && value <= 8) {
       $("#result").text("C#");
+      $(".result").show();
     } else if (value >= 9 && value <= 12) {
-      $("#result").text("Python"); 
+      $("#result").text("Python");
+      $(".result").show();
     } else {
       $("#result").text("javascript");
+      $(".result").show();
     }
     event.preventDefault();
   });
